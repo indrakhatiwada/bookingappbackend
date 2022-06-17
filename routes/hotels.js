494f41740cf3ba1d16265a48d8e7661b54be7277
Indra Khatiwada <32447984+indrakhatiwada@users.noreply.github.com>
2022-06-17 18:29:05 +0545

    added error handlers

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "./errorFile.js";
 
 
 const router = express.Router();
@@ -20,12 +21,12 @@ router.post("/", async (req,res)=>{
 //update
 router.put("/:id", async (req,res)=>{
     //passing req params so that it can find particular
-    //hotel by if
+    //hotel by id
     try{
         //finds hotel and updates
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id)
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
 
-        res.status(200).json(savedHotel)
+        res.status(200).json(updatedHotel)
 
     }catch(err){
         res.status(500).json(err)
@@ -33,8 +34,51 @@ router.put("/:id", async (req,res)=>{
 
 })
 
-//get
+//find by id and delete
+router.delete("/:id", async (req,res)=>{
+    //passing req params so that it can find particular
+    //hotel by id
+    try{   
+        await Hotel.findByIdAndDelete(req.params.id) ;    
+
+        res.status(200).json("Hotel deleted")
+
+    }catch(err){
+        next(err)
+    }
+
+})
+
+//find hotel by id
+
+router.get("/:id", async (req,res,next)=>{
+    //passing req params so that it can find particular
+    //hotel by id
+    
+    try{
+        //finds hotel and updates
+        const hotel = await Hotel.findById(req.params.id)
+
+        res.status(200).json(hotel)
+
+    }catch(err){
+        next(err);
+    }
+
+})
+
+
 //get all
+router.get("/", async (req,res)=>{ 
+   
+    
+    try{
+        const hotels =  await Hotel.find()
+        res.status(200).json(hotels)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 
 export default router;
